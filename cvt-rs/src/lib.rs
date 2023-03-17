8e1f06bb869aa85b9c44476164fa2fc0f8af6652
Lena <me@lena.place>
2023-03-17 11:22:29 +0100

    cargo fmt

--- cvt-rs/src/lib.rs
+++ cvt-rs/src/lib.rs
@@ -1,28 +1,28 @@
-use std::fmt::{Display};
+use std::fmt::Display;
 
 #[derive(Debug, Clone, Copy, PartialEq)]
 pub struct CvtTimings {
     pixel_clock: f64, // MHz
 
-    h_total: u32, // Pixels
-    h_active: u32, // Pixels
-    h_blank: u32, // Pixels
-    h_front_porch: u32, // Pixels
-    h_sync: u32, // Pixels
-    h_back_porch: u32, // Pixels
+    h_total: u32,          // Pixels
+    h_active: u32,         // Pixels
+    h_blank: u32,          // Pixels
+    h_front_porch: u32,    // Pixels
+    h_sync: u32,           // Pixels
+    h_back_porch: u32,     // Pixels
     h_sync_polarity: bool, // +/-
-    h_freq: f64, // KHz
-    h_period: f64, // us
+    h_freq: f64,           // KHz
+    h_period: f64,         // us
 
-    v_total: u32, // Pixels
-    v_active: u32, // Pixels
-    v_blank: u32, // Pixels
-    v_front_porch: u32, // Pixels
-    v_sync: u32, // Pixels
-    v_back_porch: u32, // Pixels
+    v_total: u32,          // Pixels
+    v_active: u32,         // Pixels
+    v_blank: u32,          // Pixels
+    v_front_porch: u32,    // Pixels
+    v_sync: u32,           // Pixels
+    v_back_porch: u32,     // Pixels
     v_sync_polarity: bool, // +/-
-    v_freq: f64, // KHz
-    v_period: f64, // us
+    v_freq: f64,           // KHz
+    v_period: f64,         // us
 
     interlaced: bool,
 }
@@ -31,7 +31,7 @@ pub struct CvtTimings {
 pub enum BlankingMode {
     Normal,
     Reduced,
-    ReducedV2
+    ReducedV2,
 }
 
 enum AspectRatio {
@@ -40,19 +40,26 @@ enum AspectRatio {
     Aspect16by10,
     Aspect5by4,
     Aspect15by9,
-    
+
     // not defined by the spec
     Aspect43by18,
     Aspect64by27,
     Aspect12by5,
-    AspectUnknown
+    AspectUnknown,
 }
 
 impl CvtTimings {
     // generates  video timings according to the VESA CVT standard
     // look into GTF for the future
     // https://glenwing.github.io/docs/VESA-GTF-1.1.pdf
-    pub fn generate(h_pixels: u32, v_pixels: u32, refresh_rate: f64, blanking_mode: BlankingMode, margins: bool, interlaced: bool) -> Result<Self, anyhow::Error> {
+    pub fn generate(
+        h_pixels: u32,
+        v_pixels: u32,
+        refresh_rate: f64,
+        blanking_mode: BlankingMode,
+        margins: bool,
+        interlaced: bool,
+    ) -> Result<Self, anyhow::Error> {
         let clock_step: f64;
         let min_v_bporch: u32 = 6;
         let rb_h_blank: u32;
@@ -101,52 +108,68 @@ impl CvtTimings {
 
         // 5.2 Computation of Common Parameters
         // if interlacing, set it to 2x the refresh rate
-        let v_field_rate_rqd = if interlaced { refresh_rate * 2.0 } else { refresh_rate };
+        let v_field_rate_rqd = if interlaced {
+            refresh_rate * 2.0
+        } else {
+            refresh_rate
+        };
 
         let h_pixels_rnd = (h_pixels as f64 / cell_gran_rnd).floor() * cell_gran_rnd;
 
         // if using margins, set the margin
-        let left_margin = if margins {((h_pixels_rnd * margin_per / 100.0) / cell_gran_rnd).floor() * cell_gran_rnd} else {0.0};
+        let left_margin = if margins {
+            ((h_pixels_rnd * margin_per / 100.0) / cell_gran_rnd).floor() * cell_gran_rnd
+        } else {
+            0.0
+        };
         let right_margin = left_margin;
 
         let total_active_pixels = (h_pixels_rnd + left_margin + right_margin).floor() as u32; // floor?
 
-        let v_lines_rnd = if interlaced { ((v_pixels as f64) / 2.0).floor() } else { (v_pixels as f64).floor() };
-        
-        let top_margin = if margins {(v_lines_rnd * margin_per / 100.0).floor()} else {0.0};
+        let v_lines_rnd = if interlaced {
+            ((v_pixels as f64) / 2.0).floor()
+        } else {
+            (v_pixels as f64).floor()
+        };
+
+        let top_margin = if margins {
+            (v_lines_rnd * margin_per / 100.0).floor()
+        } else {
+            0.0
+        };
         let bot_margin = top_margin;
 
-        let interlace = if interlaced {0.5} else {0.0};
-
+        let interlace = if interlaced { 0.5 } else { 0.0 };
 
         let v_sync_rnd: f64;
 
         if blanking_mode == BlankingMode::ReducedV2 {
             v_sync_rnd = 8.0;
         } else {
-        // calculate aspect ratio
-        let aspect_ratio = get_aspect_ratio(interlaced, v_lines_rnd, h_pixels_rnd, cell_gran_rnd);
-        
-        match aspect_ratio {
-            AspectRatio::Aspect4by3 => {
-                v_sync_rnd = 4.0;
+            // calculate aspect ratio
+            let aspect_ratio =
+                get_aspect_ratio(interlaced, v_lines_rnd, h_pixels_rnd, cell_gran_rnd);
+
+            match aspect_ratio {
+                AspectRatio::Aspect4by3 => {
+                    v_sync_rnd = 4.0;
+                }
+                AspectRatio::Aspect16by9 => {
+                    v_sync_rnd = 5.0;
+                }
+                AspectRatio::Aspect16by10 => {
+                    v_sync_rnd = 6.0;
+                }
+                AspectRatio::Aspect5by4 => {
+                    v_sync_rnd = 7.0;
+                }
+                AspectRatio::Aspect15by9 => {
+                    v_sync_rnd = 7.0;
+                }
+                _ => {
+                    v_sync_rnd = 10.0;
+                }
             }
-            AspectRatio::Aspect16by9 => {
-                v_sync_rnd = 5.0;
-            }
-            AspectRatio::Aspect16by10 => {
-                v_sync_rnd = 6.0;
-            }
-            AspectRatio::Aspect5by4 => {
-                v_sync_rnd = 7.0;
-            }
-            AspectRatio::Aspect15by9 => {
-                v_sync_rnd = 7.0;
-            }
-            _ => {
-                v_sync_rnd = 10.0;
-            }
-        }
         }
 
         let h_period_est: f64;
@@ -163,38 +186,56 @@ impl CvtTimings {
         let h_front_porch: f64;
         let act_pix_freq: f64;
         if blanking_mode == BlankingMode::Normal {
-           h_period_est = ((1.0 / v_field_rate_rqd) - min_vsync_bp / 1000000.0) / (v_lines_rnd + (2.0 * top_margin) + min_v_porch_rnd + interlace) * 1000000.0;
-           v_sync_bp = (min_vsync_bp / h_period_est).floor() + 1.0;
-           
-           if v_sync_bp < (v_sync_rnd + min_v_bporch as f64) {
-            v_sync_bp = v_sync_rnd + min_v_bporch as f64;
-           }
-           v_blank = v_sync_bp + min_v_porch_rnd;
-           v_front_porch = min_v_porch_rnd;
-           v_back_porch = v_sync_bp - v_sync_rnd;
-           total_v_lines = v_lines_rnd + top_margin + bot_margin + v_sync_bp + interlace + min_v_porch_rnd;
-           ideal_duty_cycle = c_prime as f64 - (m_prime as f64 * h_period_est/1000.0);
+            h_period_est = ((1.0 / v_field_rate_rqd) - min_vsync_bp / 1000000.0)
+                / (v_lines_rnd + (2.0 * top_margin) + min_v_porch_rnd + interlace)
+                * 1000000.0;
+            v_sync_bp = (min_vsync_bp / h_period_est).floor() + 1.0;
 
-           if ideal_duty_cycle < 20.0 {
-            h_blank = (total_active_pixels as f64 * 20.0 / (100.0-20.0) / (2.0 * cell_gran_rnd)) * (2.0 * cell_gran_rnd);
-           } else {
-            h_blank = (total_active_pixels as f64 * ideal_duty_cycle / (100.0 - ideal_duty_cycle) / (2.0 * cell_gran_rnd)).floor() * (2.0 * cell_gran_rnd);
-           }
-           total_pixels = total_active_pixels as f64 + h_blank;
+            if v_sync_bp < (v_sync_rnd + min_v_bporch as f64) {
+                v_sync_bp = v_sync_rnd + min_v_bporch as f64;
+            }
+            v_blank = v_sync_bp + min_v_porch_rnd;
+            v_front_porch = min_v_porch_rnd;
+            v_back_porch = v_sync_bp - v_sync_rnd;
+            total_v_lines =
+                v_lines_rnd + top_margin + bot_margin + v_sync_bp + interlace + min_v_porch_rnd;
+            ideal_duty_cycle = c_prime as f64 - (m_prime as f64 * h_period_est / 1000.0);
 
-           h_sync = (h_sync_per * total_pixels / cell_gran_rnd).floor() * cell_gran_rnd;
-           h_back_porch = h_blank / 2.0;
-           h_front_porch = h_blank - h_sync - h_back_porch;
-           act_pix_freq = clock_step * (total_pixels / h_period_est / clock_step).floor();
+            if ideal_duty_cycle < 20.0 {
+                h_blank =
+                    (total_active_pixels as f64 * 20.0 / (100.0 - 20.0) / (2.0 * cell_gran_rnd))
+                        * (2.0 * cell_gran_rnd);
+            } else {
+                h_blank = (total_active_pixels as f64 * ideal_duty_cycle
+                    / (100.0 - ideal_duty_cycle)
+                    / (2.0 * cell_gran_rnd))
+                    .floor()
+                    * (2.0 * cell_gran_rnd);
+            }
+            total_pixels = total_active_pixels as f64 + h_blank;
+
+            h_sync = (h_sync_per * total_pixels / cell_gran_rnd).floor() * cell_gran_rnd;
+            h_back_porch = h_blank / 2.0;
+            h_front_porch = h_blank - h_sync - h_back_porch;
+            act_pix_freq = clock_step * (total_pixels / h_period_est / clock_step).floor();
         } else {
-            h_period_est = ((1000000.0 / v_field_rate_rqd) - rb_min_v_blank as f64) / (v_lines_rnd + top_margin + bot_margin);
+            h_period_est = ((1000000.0 / v_field_rate_rqd) - rb_min_v_blank as f64)
+                / (v_lines_rnd + top_margin + bot_margin);
             h_blank = rb_h_blank as f64;
             let vbi_lines = (rb_min_v_blank as f64 / h_period_est) + 1.0;
             let rb_min_vbi = rb_v_fporch as f64 + v_sync_rnd + min_v_bporch as f64;
-            let act_vbi_lines = if vbi_lines < rb_min_vbi {rb_min_vbi} else {vbi_lines};
+            let act_vbi_lines = if vbi_lines < rb_min_vbi {
+                rb_min_vbi
+            } else {
+                vbi_lines
+            };
             total_v_lines = act_vbi_lines + v_lines_rnd + top_margin + bot_margin + interlace;
             total_pixels = (rb_h_blank + total_active_pixels) as f64;
-            act_pix_freq = clock_step * ((v_field_rate_rqd * total_v_lines * total_pixels / 1000000.0 * refresh_multiplier) / clock_step).floor();
+            act_pix_freq = clock_step
+                * ((v_field_rate_rqd * total_v_lines * total_pixels / 1000000.0
+                    * refresh_multiplier)
+                    / clock_step)
+                    .floor();
 
             if blanking_mode == BlankingMode::ReducedV2 {
                 v_blank = act_vbi_lines;
@@ -214,41 +255,75 @@ impl CvtTimings {
             }
         }
 
-
         let pclock = act_pix_freq * 1000000.0;
         let h_freq = pclock / total_pixels;
         let v_freq = pclock / (total_v_lines * total_pixels);
-     Ok(Self {
-         pixel_clock: pclock,
-         h_active: total_active_pixels,
-         h_blank: h_blank as u32,
-         h_total: total_pixels as u32,
-         v_active: v_lines_rnd as u32,
-         v_blank: v_blank as u32,
-         v_total: total_v_lines as u32,
-         h_freq: h_freq.round(), //todo: round to 2 decimal places
-         v_freq: v_freq.round(), // here as well
-         h_period: 1.0/h_freq,
-         v_period: 1.0/v_freq,
-         h_front_porch: h_front_porch as u32,
-         h_sync: h_sync as u32,
-         h_back_porch: h_back_porch as u32,
-         h_sync_polarity: h_pol,
-         v_front_porch: v_front_porch as u32,
-         v_sync: v_sync_rnd as u32,
-         v_back_porch: v_back_porch as u32,
-         v_sync_polarity: v_pol,
-         interlaced
-     })
+        Ok(Self {
+            pixel_clock: pclock,
+            h_active: total_active_pixels,
+            h_blank: h_blank as u32,
+            h_total: total_pixels as u32,
+            v_active: v_lines_rnd as u32,
+            v_blank: v_blank as u32,
+            v_total: total_v_lines as u32,
+            h_freq: h_freq.round(), //todo: round to 2 decimal places
+            v_freq: v_freq.round(), // here as well
+            h_period: 1.0 / h_freq,
+            v_period: 1.0 / v_freq,
+            h_front_porch: h_front_porch as u32,
+            h_sync: h_sync as u32,
+            h_back_porch: h_back_porch as u32,
+            h_sync_polarity: h_pol,
+            v_front_porch: v_front_porch as u32,
+            v_sync: v_sync_rnd as u32,
+            v_back_porch: v_back_porch as u32,
+            v_sync_polarity: v_pol,
+            interlaced,
+        })
     }
 
     pub fn generate_modeline(&self) -> String {
-        format!("Modeline \"{}x{}_{}{}\" {} {} {} {} {} {} {} {} {} {} {} {}", self.h_active, self.v_active, self.v_freq, if self.interlaced {"i"} else {""}, (self.pixel_clock/1000.0).round() / 1000.0, self.h_active, self.h_active + self.h_front_porch, self.h_active + self.h_front_porch + self.h_sync, self.h_total, self.v_active, self.v_active + self.v_front_porch, self.v_active + self.v_front_porch + self.v_sync, self.v_total, if self.h_sync_polarity {"+HSync"} else {"-HSync"}, if self.v_sync_polarity {"+Vsync"} else {"-VSync"}, if self.interlaced {"Interlace"} else {""})
+        format!(
+            "Modeline \"{}x{}_{}{}\" {} {} {} {} {} {} {} {} {} {} {} {}",
+            self.h_active,
+            self.v_active,
+            self.v_freq,
+            if self.interlaced { "i" } else { "" },
+            (self.pixel_clock / 1000.0).round() / 1000.0,
+            self.h_active,
+            self.h_active + self.h_front_porch,
+            self.h_active + self.h_front_porch + self.h_sync,
+            self.h_total,
+            self.v_active,
+            self.v_active + self.v_front_porch,
+            self.v_active + self.v_front_porch + self.v_sync,
+            self.v_total,
+            if self.h_sync_polarity {
+                "+HSync"
+            } else {
+                "-HSync"
+            },
+            if self.v_sync_polarity {
+                "+Vsync"
+            } else {
+                "-VSync"
+            },
+            if self.interlaced { "Interlace" } else { "" }
+        )
     }
 }
 
-fn get_aspect_ratio(interlaced: bool, v_lines_rnd: f64, h_pixels_rnd: f64, cell_gran_rnd: f64) -> AspectRatio {
-    let ver_pixels = if interlaced {2.0 * v_lines_rnd} else {v_lines_rnd};
+fn get_aspect_ratio(
+    interlaced: bool,
+    v_lines_rnd: f64,
+    h_pixels_rnd: f64,
+    cell_gran_rnd: f64,
+) -> AspectRatio {
+    let ver_pixels = if interlaced {
+        2.0 * v_lines_rnd
+    } else {
+        v_lines_rnd
+    };
     let hor_pixels_4_3 = cell_gran_rnd * (ver_pixels * 4.0 / 3.0).floor() / cell_gran_rnd;
     let hor_pixels_16_9 = cell_gran_rnd * (ver_pixels * 16.0 / 9.0).floor() / cell_gran_rnd;
     let hor_pixels_16_10 = cell_gran_rnd * (ver_pixels * 16.0 / 10.0).floor() / cell_gran_rnd;
--- overdrivectl/src/main.rs
+++ overdrivectl/src/main.rs
@@ -1,8 +1,15 @@
 fn main() {
     println!("Hello, world!");
-    let a = cvt_rs::CvtTimings::generate(1280, 1024, 79.0, cvt_rs::BlankingMode::ReducedV2, false, false).unwrap();
+    let a = cvt_rs::CvtTimings::generate(
+        1280,
+        1024,
+        79.0,
+        cvt_rs::BlankingMode::ReducedV2,
+        false,
+        false,
+    )
+    .unwrap();
 
     dbg!(&a);
     println!("{}", a.generate_modeline());
-
 }
